672417f55f38ae0ea99b4803d5032dfff936951e
Fernando Sayago <fsayago@idsync.com>
2020-05-04 10:32:11 -0400

    Add project files.

---
+++ WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.registrationButton = New System.Windows.Forms.Button()
+        Me.executionButton = New System.Windows.Forms.Button()
+        Me.installationButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'registrationButton
+        '
+        Me.registrationButton.Location = New System.Drawing.Point(41, 69)
+        Me.registrationButton.Name = "registrationButton"
+        Me.registrationButton.Size = New System.Drawing.Size(147, 23)
+        Me.registrationButton.TabIndex = 1
+        Me.registrationButton.Text = "Test Registration"
+        Me.registrationButton.UseVisualStyleBackColor = True
+        '
+        'executionButton
+        '
+        Me.executionButton.Location = New System.Drawing.Point(41, 302)
+        Me.executionButton.Name = "executionButton"
+        Me.executionButton.Size = New System.Drawing.Size(147, 23)
+        Me.executionButton.TabIndex = 2
+        Me.executionButton.Text = "Test Execution"
+        Me.executionButton.UseVisualStyleBackColor = True
+        '
+        'installationButton
+        '
+        Me.installationButton.Location = New System.Drawing.Point(41, 164)
+        Me.installationButton.Name = "installationButton"
+        Me.installationButton.Size = New System.Drawing.Size(147, 23)
+        Me.installationButton.TabIndex = 3
+        Me.installationButton.Text = "Test Installation"
+        Me.installationButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.installationButton)
+        Me.Controls.Add(Me.executionButton)
+        Me.Controls.Add(Me.registrationButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents registrationButton As Button
+    Friend WithEvents executionButton As Button
+    Friend WithEvents installationButton As Button
+End Class
---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports System.Net.Http, Newtonsoft.Json, System.Text, System.Net.Http.Headers
+
+Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles registrationButton.Click
+        Dim Today
+        Today = Now
+        Dim data As New With {
+            .ActivationKey = "1234567890",
+            .Customer = "Challenger",
+            .CreationDate = Today,
+            .MachineName = "TSI-271",
+            .Product = "Tranzactor Integration Suite",
+            .SerialNumber = "1C92C284",
+            .UserName = "TRANZTEC\Toby",
+            .Version = "2020.0.450.0",
+            .Comment = "I JUST SENT YOU THIS ONE"
+        }
+        Dim url As String = "https://register.tranzactor.com/registration"
+        Dim content As HttpContent = New ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data)))
+        content.Headers.ContentType = New MediaTypeWithQualityHeaderValue("application/json")
+        Using client As New HttpClient
+            Try
+                client.DefaultRequestHeaders.Add("integrationCode", "Zc#yB3zwF&uV6&Y&x533@vqnvwyUP%Atk$Z4cqm#")
+                Dim task As Task(Of HttpResponseMessage) = client.PostAsync(url, content)
+                task.Wait()
+                MessageBox.Show("Response status: " & task.Result.StatusCode & " :: " & task.Result.ReasonPhrase)
+            Catch ex As Exception
+                MessageBox.Show("Error updating registration DB: " & ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles installationButton.Click
+        Dim Today
+        Today = Now
+        Dim data As New With {
+            .ActivationKey = "1234567890",
+            .InstallDate = Today,
+            .SerialNumber = "1C92C284",
+            .Product = "Tranzactor Integration Suite",
+            .Version = "2020.0.450.0",
+            .InstallUser = "TRANZTEC\Toby",
+            .InstallMachineName = "TSI-270",
+            .Status = "Install",
+            .Type = "Tranzactor Integration Suite",
+            .Comment = "Test commentTest commentTest commentTest commentTest"
+        }
+        Dim url As String = "https://register.tranzactor.com/installation"
+        Dim content As HttpContent = New ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data)))
+        content.Headers.ContentType = New MediaTypeWithQualityHeaderValue("application/json")
+        Using client As New HttpClient
+            Try
+                client.DefaultRequestHeaders.Add("integrationCode", "kms8Y#45XkHujnEQ%@^vzXb2!u67m%D7Mv3QU7B@")
+                Dim task As Task(Of HttpResponseMessage) = client.PostAsync(url, content)
+                task.Wait()
+                MessageBox.Show("Response status: " & task.Result.StatusCode & " :: " & task.Result.ReasonPhrase)
+            Catch ex As Exception
+                MessageBox.Show("Error updating registration DB: " & ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles executionButton.Click
+        Dim Today
+        Today = Now
+        Dim data As New With {
+            .RunAsUser = "Toby",
+            .RunDate = Today,
+            .Status = "Started",
+            .Product = "Tranzactor Integration Suite",
+            .TranzactorVersion = "2020.0.450.0",
+            .ActivationKey = "1234567890",
+            .SerialNumber = "1C92C284",
+            .MachineName = "TSI-271",
+            .OSLevel = "",
+            .DriveSize = "",
+            .DriveSpaceFree = "",
+            .DBSize = "",
+            .QueueDetail = "{
+               ""MobileComTrip"" = 6,
+                ""MobileComFormMessage"" = 14,
+                ""MobileComMessage"" = 25,
+                ""MobileComPositionReport"" = 15492
+             }",
+            .Schemas = "",
+            .Config = "",
+            .Type = "Tranzactor Integration Suite",
+            .Comment = "Test commentTest commentTest commentTest commentTest commentTest commentTest commentTest commentTest commentTest commentTest commentTest comment"
+        }
+        Dim url As String = "https://register.tranzactor.com/execution"
+        Dim content As HttpContent = New ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data)))
+        content.Headers.ContentType = New MediaTypeWithQualityHeaderValue("application/json")
+        Using client As New HttpClient
+            Try
+                client.DefaultRequestHeaders.Add("integrationCode", "mPnW$B28EM@v2f32R3rD5uG*XmME@YHeST9*n@QK")
+                Dim task As Task(Of HttpResponseMessage) = client.PostAsync(url, content)
+                task.Wait()
+                MessageBox.Show("Response status: " & task.Result.StatusCode & " :: " & task.Result.ReasonPhrase)
+            Catch ex As Exception
+                MessageBox.Show("Error updating registration DB: " & ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End Using
+    End Sub
+End Class
